d0821bc0308117a857a5f39e120a1c7379a71c22
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-06-06 21:59:57 -0700

    Add basic functionality to take location and make/log weather request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./styles.css";
+import { getWeatherData } from "./modules/weatherAPI.js";
 
-console.log("Weather app loaded");
+const searchForm = document.querySelector(".search-form");
+
+async function handleSearch(e) {
+  e.preventDefault();
+  try {
+    const formData = new FormData(searchForm);
+    const searchValue = formData.get("location").trim();
+    if (!searchValue) {
+      throw new Error("No location entered");
+    }
+    const weatherData = await getWeatherData(searchValue);
+
+    console.log(weatherData);
+    searchForm.reset();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+searchForm.addEventListener("submit", handleSearch);
---
+++ src/modules/weatherAPI.js
@@ -0,0 +1,10 @@
+export async function getWeatherData(location) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=us&contentType=json&key=${"CW8W2BVAPWP334DQ26VVLBVFF"}`,
+  );
+  if (!response.ok) {
+    throw new Error(`Weather request failed: ${response.status}`);
+  }
+  const json = await response.json();
+  return json;
+}
